86063e7f7edb63bb4702bbd274f9f177a7bb50f7
GwennLeRoch <gwennleroch@gmail.com>
2018-04-01 14:24:52 +0200

    predictionLoyer.py : debut comparaison

--- MachineLearning/Tuto/predictionLoyer.py
+++ MachineLearning/Tuto/predictionLoyer.py
@@ -19,7 +19,7 @@ y = np.matrix(houseData['price']).T
 print(n,X.shape,y.shape)
 
 # Separation des training/testing set
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.9)
 
 
 
@@ -29,16 +29,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
 regr = linear_model.LinearRegression()
 regr.fit(X_train, y_train)
 y_predict = regr.predict(X_test)
-error_reg = regr.score(X_test, y_test)*100
+error_reg = 100*(1-regr.score(X_test, y_test))
 print('Erreur regression linéaire : %f' %error_reg)
 
 # Classifieur k-NN
 errors = []
-for k in range(10,20):
+for k in range(2,15):
     knn = neighbors.KNeighborsClassifier(k)
     errors.append(100*(1-knn.fit(X_train, y_train).score(X_test, y_test)))
 
 # Comparaison des modeles
-plt.plot([10,20],[error_reg,error_reg])
-plt.plot(range(10,20), errors, 'o-')
+plt.plot([2,15],[error_reg,error_reg])
+plt.plot(range(2,15), errors, 'o-')
 plt.show()
